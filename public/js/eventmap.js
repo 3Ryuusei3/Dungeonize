b70ab0cdfbe43c8655526165885f8d30dac1b521
Manuel Atance <ryuusei3@MacBook-Pro-de-Manuel.local>
2022-11-23 13:57:57 +0100

    Mapas mejorados y diseño página inicial empezado

--- public/js/eventmap.js
+++ public/js/eventmap.js
@@ -2,27 +2,36 @@ let eventMap;
 let eventId;
 
 function init() {
-	renderMap();
 	getEventId();
 	getEventLocation();
 }
 
-function renderMap() {
-	eventMap = new google.maps.Map(document.getElementById("eventMap"), {
-		zoom: 13,
-		center: { lat: 40.41328570929002, lng: -3.697121041240825 },
-	});
-}
-
 function getEventId() {
 	eventId = location.pathname.split("/");
+	while (eventId.length > 1) {
+		eventId.shift()
+	};
 }
 
 function getEventLocation() {
 	axios
-		.get(`/api/events/${eventId[3]}`)
-		.then((res) => setMarkers(res.data))
+		.get(`/api/events/${eventId}`)
+		.then((res) => renderMap(res.data))
 		.catch((err) => console.log(err));
+}
+
+function renderMap(event) {
+	const lat = event.location.coordinates[0];
+	const lng = event.location.coordinates[1];
+
+	eventMap = new google.maps.Map(
+		document.getElementById("eventMap"),
+		{
+			zoom: 13,
+			center: { lat, lng }
+		}
+	)
+	setMarkers(event)
 }
 
 function setMarkers(event) {
